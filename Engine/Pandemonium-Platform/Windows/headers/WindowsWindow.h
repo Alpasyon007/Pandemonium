54b4176b75c0f3267e7eb422477e9c1d838dbeb0
Alpasyon007 <alpasyon07@gmail.com>
2022-11-06 05:26:50 +0000

    Add Input Polling

--- Engine/Pandemonium-Platform/Windows/headers/WindowsWindow.h
+++ Engine/Pandemonium-Platform/Windows/headers/WindowsWindow.h
@@ -12,14 +12,18 @@ namespace Pandemonium {
 		WindowsWindow(const WindowProps& props);
 		virtual ~WindowsWindow();
 
-		void				OnUpdate() override;
+		void				 OnUpdate() override;
 
-		inline unsigned int GetWidth() const override { return m_Data.Width; }
-		inline unsigned int GetHeight() const override { return m_Data.Height; }
+		inline unsigned int	 GetWidth() const override { return m_Data.Width; }
 
-		inline void			SetEventCallback(const EventCallbackFn& callback) override { m_Data.EventCallback = callback; }
-		void				SetVSync(bool enabled) override;
-		bool				IsVSync() const override;
+		inline unsigned int	 GetHeight() const override { return m_Data.Height; }
+
+		inline void			 SetEventCallback(const EventCallbackFn& callback) override { m_Data.EventCallback = callback; }
+
+		void				 SetVSync(bool enabled) override;
+		bool				 IsVSync() const override;
+
+		inline virtual void* GetNativeWindow() const { return m_Window; };
 	private:
 		virtual void Init(const WindowProps& props);
 		virtual void Shutdown();
