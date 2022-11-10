56710e5fa66658c96181d541aefd131a8436cd5c
Alpasyon007 <alpasyon07@gmail.com>
2022-11-10 01:59:39 +0000

    Implemented Vertex Buffer Layouts

--- Engine/Pandemonium-Platform/OpenGL/headers/OpenGLBuffer.h
+++ Engine/Pandemonium-Platform/OpenGL/headers/OpenGLBuffer.h
@@ -10,12 +10,17 @@ namespace Pandemonium {
 
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void		 Bind() const;
-		virtual void		 Unbind() const;
+		virtual void				Bind() const override;
+		virtual void				Unbind() const override;
 
-		static VertexBuffer* Create(float* vertices, uint32_t size);
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; }
+
+		virtual void				SetLayout(const BufferLayout& layout) override { m_Layout = layout; };
+
+		static VertexBuffer*		Create(float* vertices, uint32_t size);
 	private:
-		uint32_t m_RendererID;
+		uint32_t	 m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer {
@@ -24,8 +29,8 @@ namespace Pandemonium {
 
 		virtual ~OpenGLIndexBuffer();
 
-		virtual void		Bind() const;
-		virtual void		Unbind() const;
+		virtual void		Bind() const override;
+		virtual void		Unbind() const override;
 
 		virtual uint32_t	GetCount() const { return m_Count; }
 
