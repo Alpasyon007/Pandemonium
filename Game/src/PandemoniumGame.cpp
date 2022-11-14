d7baa19952813ce21e6b6bc2d6e6cb180c76e51d
Alpasyon007 <alpasyon07@gmail.com>
2022-11-14 04:23:43 +0000

    Implement very basic transforms

--- Game/src/PandemoniumGame.cpp
+++ Game/src/PandemoniumGame.cpp
@@ -2,6 +2,8 @@
 
 #include "imgui.h"
 
+#include "glm/gtc/matrix_transform.hpp"
+
 class ExampleLayer : public Pandemonium::Layer {
 public:
 	ExampleLayer() : Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f) {
@@ -24,7 +26,7 @@ public:
 
 		m_SquareVA.reset(Pandemonium::VertexArray::Create());
 
-		float									   squareVertices[3 * 4] = {-0.75f, -0.75f, 0.0f, 0.75f, -0.75f, 0.0f, 0.75f, 0.75f, 0.0f, -0.75f, 0.75f, 0.0f};
+		float									   squareVertices[3 * 4] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.5f, 0.5f, 0.0f, -0.5f, 0.5f, 0.0f};
 
 		std::shared_ptr<Pandemonium::VertexBuffer> squareVB;
 		squareVB.reset(Pandemonium::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
@@ -43,6 +45,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -50,7 +53,7 @@ public:
 			void main() {
 				v_Position = a_Position; 
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -78,12 +81,13 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 
 			void main() {
 				v_Position = a_Position; 
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -128,7 +132,16 @@ public:
 
 		Pandemonium::Renderer::BeginScene(m_Camera);
 
-		Pandemonium::Renderer::Submit(m_BlueShader, m_SquareVA);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for(int y = 0; y < 20; y++) {
+			for(int x = 0; x < 20; x++) {
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Pandemonium::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
+
 		Pandemonium::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Pandemonium::Renderer::EndScene();
