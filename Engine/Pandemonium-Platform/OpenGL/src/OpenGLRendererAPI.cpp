9568e4651e9a19b627315ecc4f32bb09070b7ce3
Alpasyon007 <alpasyon07@gmail.com>
2022-11-23 17:57:17 +0000

    Implement window resizing

--- Engine/Pandemonium-Platform/OpenGL/src/OpenGLRendererAPI.cpp
+++ Engine/Pandemonium-Platform/OpenGL/src/OpenGLRendererAPI.cpp
@@ -9,6 +9,8 @@ namespace Pandemonium {
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	}
 
+	void OpenGLRendererAPI::SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) { glViewport(x, y, width, height); }
+
 	void OpenGLRendererAPI::SetClearColor(const glm::vec4& color) { glClearColor(color.r, color.g, color.b, color.a); }
 
 	void OpenGLRendererAPI::Clear() { glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); }
