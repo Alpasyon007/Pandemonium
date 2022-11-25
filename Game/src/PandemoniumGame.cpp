999dfd4cb093b78b94073e8cbf483274d6a2d1c4
Alpasyon007 <alpasyon07@gmail.com>
2022-11-25 03:08:40 +0000

    Code cleanup

--- Game/src/PandemoniumGame.cpp
+++ Game/src/PandemoniumGame.cpp
@@ -185,20 +185,4 @@ public:
 	~PandemoniumGame() {}
 };
 
-Pandemonium::Application* Pandemonium::CreateApplication() {
-	// int a = 5;
-	// char* b = "BYE";
-
-	// ASSERT(a < 1);
-	// ASSERT(a < 1, "HELLO");
-	// ASSERT(a < 1, "HELLO %d", a);
-
-	// // DEBUG_BREAK();
-
-	// LOG_DEBUG("HELLO %d %s", a, b);
-	// LOG_INFO("HELLO %d %s", a, b);
-	// LOG_WARN("HELLO %d %s", a, b);
-	// LOG_ERROR("HELLO %d %s", a, b);
-
-	return new PandemoniumGame();
-}
+Pandemonium::Application* Pandemonium::CreateApplication() { return new PandemoniumGame(); }
