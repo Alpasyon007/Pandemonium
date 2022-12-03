2595aebad4f0014392ca7199d9bd16b49682d3f2
Alpasyon007 <alpasyon07@gmail.com>
2022-12-03 02:48:05 +0000

    Merge colour shader and texture shader into one

--- Engine/Pandemonium-Platform/OpenGL/headers/OpenGLTexture.h
+++ Engine/Pandemonium-Platform/OpenGL/headers/OpenGLTexture.h
@@ -2,11 +2,13 @@
 #define OPEN_GL_TEXTURE_H
 
 #include "Texture.h"
+#include <glad/glad.h>
 
 namespace Pandemonium {
 
 	class OpenGLTexture2D : public Texture2D {
 	public:
+		OpenGLTexture2D(uint32_t width, uint32_t height);
 		OpenGLTexture2D(const std::string& path);
 		virtual ~OpenGLTexture2D();
 
@@ -14,11 +16,14 @@ namespace Pandemonium {
 
 		virtual uint32_t GetHeight() const override { return m_Height; };
 
+		virtual void	 SetData(void* data, uint32_t size) override;
+
 		virtual void	 Bind(uint32_t slot = 0) const override;
 	private:
 		std::string m_Path;
 		uint32_t	m_Width, m_Height;
 		uint32_t	m_RendererID;
+		GLenum		m_InternalFormat, m_DataFormat;
 	};
 
 } // namespace Pandemonium
